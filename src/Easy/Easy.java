89c7c2567fba33b4e4a43477457a997e7aca8f42
John Lazzarini <jlazzari@mail.sfsu.edu>
2020-04-06 23:28:10 -0700

    Initial commit

---
+++ src/Easy/Easy.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Easy {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World!");
+    }
+
+}
